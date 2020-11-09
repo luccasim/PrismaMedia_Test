3dd52f8cdab099ae63dff57b578feaaceb484474
Luc Casimir <casimir.luc@gmail.com>
2020-11-09 10:33:26 +0100

    + Create storyboards Controllers + Add UserViewControllers + Add UserSettingViewControllers

---
+++ PrismaMedia_Test/Controllers/UserSettingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  UserSettingViewController.swift
+//  PrismaMedia_Test
+//
+//  Created by owee on 09/11/2020.
+//
+
+import UIKit
+
+class UserSettingViewController: UIViewController {
+    
+    
+    @IBOutlet weak var fullnameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var settingButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PrismaMedia_Test/Controllers/UserViewController.swift
@@ -0,0 +1,33 @@
+//
+//  UserViewController.swift
+//  PrismaMedia_Test
+//
+//  Created by owee on 09/11/2020.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+    
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var fullnameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var settingButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
