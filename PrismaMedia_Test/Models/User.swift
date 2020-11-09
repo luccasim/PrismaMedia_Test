5645f2f19a04c30f32b1872b36329d2d05889d87
Luc Casimir <casimir.luc@gmail.com>
2020-11-09 11:24:24 +0100

    + Add User Model + Add LoadingActivity

---
+++ PrismaMedia_Test/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  PrismaMedia_Test
+//
+//  Created by owee on 09/11/2020.
+//
+
+import Foundation
+
+struct User {
+    let fullName : String
+    let email : String
+    let avatarURL : URL?
+}
